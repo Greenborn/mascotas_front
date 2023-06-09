b68fb2e3be9b8bbc0beafdf3fe038f5700cbeee3
Luciano <lucho.2012.tandil@gmail.com>
2023-06-09 20:23:31 -0300

    En vista home se agrega menu principal

--- src/views/dashboard/HomePage.vue
+++ src/views/dashboard/HomePage.vue
@@ -1,13 +1,37 @@
 <template>
-    <ion-grid>
-        <ion-row>
-            <ion-col>
-                HomePage
-            </ion-col>
-        </ion-row>
-    </ion-grid>
+    <ion-menu content-id="main-content">
+        <ion-header>
+        <ion-toolbar>
+            <ion-title>Animalia</ion-title>
+        </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">Este el contenido del menu</ion-content>
+    </ion-menu>
+
+    <ion-page id="main-content">
+        <ion-header>
+        <ion-toolbar>
+            <ion-buttons slot="start">
+            <ion-menu-button></ion-menu-button>
+            </ion-buttons>
+            <ion-title>Home</ion-title>
+        </ion-toolbar>
+        </ion-header>
+
+        <ion-content class="ion-padding">
+            <ion-grid>
+                <ion-row>
+                    <ion-col>
+                        
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup>
-import { IonCol, IonGrid, IonRow } from '@ionic/vue';
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonCol, IonGrid, IonRow, } from '@ionic/vue';
+import { defineComponent } from 'vue';
 </script>
--- src/views/usuario/LoginPage.vue
+++ src/views/usuario/LoginPage.vue
@@ -1,44 +1,46 @@
 <template>
-    <ion-grid>
-        <ion-row class="ion-justify-content-center ion-align-items-center vh-100">
-            <ion-col size="auto">
-                <ion-card>
-                    <ion-card-header>
-                        <ion-img src="assets/icon/icon.png" alt="Logo APP PET QR"></ion-img>
-                    </ion-card-header>
+    <ion-page id="main-content">
+        <ion-grid>
+            <ion-row class="ion-justify-content-center ion-align-items-center vh-100">
+                <ion-col size="auto">
+                    <ion-card>
+                        <ion-card-header>
+                            <ion-img src="assets/icon/icon.png" alt="Logo APP PET QR"></ion-img>
+                        </ion-card-header>
 
-                    <ion-card-content>
-                        <ion-grid>
-                            <ion-row>
-                                <ion-col>ANIMALIA</ion-col>
-                            </ion-row>
+                        <ion-card-content>
+                            <ion-grid>
+                                <ion-row>
+                                    <ion-col>ANIMALIA</ion-col>
+                                </ion-row>
 
-                            <ion-row>
-                                <ion-col><ion-input label="Usuario" placeholder="Usuario"></ion-input></ion-col>
-                            </ion-row>
-                            <ion-row>
-                                <ion-col><ion-input label="Contraseña" type="password" value="password"></ion-input></ion-col>
-                            </ion-row>
+                                <ion-row>
+                                    <ion-col><ion-input label="Usuario" placeholder="Usuario"></ion-input></ion-col>
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col><ion-input label="Contraseña" type="password" value="password"></ion-input></ion-col>
+                                </ion-row>
 
-                            <ion-row>
-                                <ion-col><a>Recuperar contraseña</a></ion-col>
-                            </ion-row>
+                                <ion-row>
+                                    <ion-col><a>Recuperar contraseña</a></ion-col>
+                                </ion-row>
 
-                            <ion-row>
-                                <ion-col><ion-button expand="full">Registrarse</ion-button></ion-col>
-                                <ion-col><ion-button expand="full" @click="login">Ingresar</ion-button></ion-col>
-                            </ion-row>
-                        </ion-grid>
-                    </ion-card-content>
-                </ion-card>
-                
-            </ion-col>
-        </ion-row>
-    </ion-grid>
+                                <ion-row>
+                                    <ion-col><ion-button expand="full">Registrarse</ion-button></ion-col>
+                                    <ion-col><ion-button expand="full" @click="login">Ingresar</ion-button></ion-col>
+                                </ion-row>
+                            </ion-grid>
+                        </ion-card-content>
+                    </ion-card>
+                    
+                </ion-col>
+            </ion-row>
+        </ion-grid>
+    </ion-page>
 </template>
 
 <script setup>
-import { IonCol, IonGrid, IonRow, IonImg, IonCard, IonCardContent, IonCardHeader  } from '@ionic/vue';
+import { IonCol, IonPage, IonGrid, IonRow, IonImg, IonCard, IonCardContent, IonCardHeader  } from '@ionic/vue';
 import { IonButton, IonInput } from '@ionic/vue';
 import { useRouter } from 'vue-router'
 
