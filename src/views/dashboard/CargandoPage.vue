091d115750999b4271bf9f9016488441b08dbe7e
Luciano <lucho.2012.tandil@gmail.com>
2023-06-09 19:21:13 -0300

    Avance con pantalla de carga

--- src/views/dashboard/CargandoPage.vue
+++ src/views/dashboard/CargandoPage.vue
@@ -1,8 +1,14 @@
 <template>
-    Pantalla de carga
-    <HomePage />
+    <ion-grid>
+        <ion-row class="ion-justify-content-center ion-align-items-center vh-100">
+            <ion-col size="auto">
+                <ion-img src="assets/icon/icon.png" alt="Logo APP PET QR"></ion-img>
+            </ion-col>
+        </ion-row>
+        
+    </ion-grid>
 </template>
 
 <script setup>
-import HomePage from './HomePage'
+import { IonCol, IonGrid, IonRow, IonImg } from '@ionic/vue';
 </script>
