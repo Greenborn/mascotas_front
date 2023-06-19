dfc3566f79353b745841bc3c88ae8fa334c8eb00
Luciano <lucho.2012.tandil@gmail.com>
2023-06-19 19:53:03 -0300

    Se hace petición de registro de usuarios

--- src/api/usuario.js
+++ src/api/usuario.js
@@ -2,10 +2,10 @@ import create from '@/helpers/request'
 
 const service = create(process.env.VUE_APP_BACKEND_URL)
 
-export function login( data ) {
-  return service({  url: "/usuario/login",    method: 'post', data  })
-}
+export function login( data ) { return service({  url: "/usuario/login",    method: 'post', data  }) }
 
-export function logout() {
-  return service({  url: "/usuario/logout", method: 'put'  })
-}
+export function registro( data ) { return service({  url: "/usuario/registro", method: 'post', data  }) }
+
+export function verifica_email( data ) { return service({  url: "/usuario/verifica_email", method: 'post', data  }) }
+
+export function logout() { return service({  url: "/usuario/logout", method: 'put'  }) }
--- src/views/usuario/RegistroUsuario.vue
+++ src/views/usuario/RegistroUsuario.vue
@@ -17,22 +17,22 @@
                             <ion-grid>                       
                                 <ion-list>
                                     <ion-item>
-                                        <ion-input label="Nombre" placeholder="Nombre"></ion-input>
+                                        <ion-input label="Nombre" placeholder="Nombre" v-model="modelo.nombre"></ion-input>
                                     </ion-item>
                                     <ion-item>
-                                        <ion-input label="E-Mail" placeholder="E-Mail"></ion-input>
+                                        <ion-input label="E-Mail" placeholder="E-Mail"  v-model="modelo.email"></ion-input>
                                     </ion-item>
                                     <ion-item>
-                                        <ion-textarea label="Descripción" placeholder="Descripción"></ion-textarea>
+                                        <ion-textarea label="Descripción" placeholder="Descripción"  v-model="modelo.descripcion"></ion-textarea>
                                     </ion-item>
                                     <ion-item>
-                                        <ion-datetime label="Fecha de Nacimiento" placeholder="Fecha de Nacimiento"></ion-datetime>
+                                        <ion-datetime label="Fecha de Nacimiento" placeholder="Fecha de Nacimiento" v-model="modelo.fecha_nacimiento"></ion-datetime>
                                     </ion-item>
                                     <ion-item>
-                                        <ion-input label="Contraseña" type="password" value="password"></ion-input>
+                                        <ion-input label="Contraseña" type="password" value="password" v-model="modelo.pass"></ion-input>
                                     </ion-item>
                                     <ion-item>
-                                        <ion-input label="Repetir Contraseña" type="password" value="password"></ion-input>
+                                        <ion-input label="Repetir Contraseña" type="password" value="password" v-model="modelo.repetir_pass"></ion-input>
                                     </ion-item>
                                 </ion-list>
                                   
@@ -46,21 +46,68 @@
                 </ion-col>
             </ion-row>
         </ion-grid>
+        <ion-loading v-if="present_loading" message="'Registrando nuevo usuario...'"> </ion-loading>
     </ion-page>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { IonItem, IonList } from '@ionic/vue' 
-import { IonCol, IonPage, IonGrid, IonRow, IonCard, IonCardContent, IonCardHeader  } from '@ionic/vue';
+import { IonCol, IonPage, IonGrid, IonRow, IonCard, IonCardContent, IonCardHeader, IonTextarea, IonDatetime } from '@ionic/vue';
 import { IonButton, IonInput } from '@ionic/vue';
 import { useRouter } from 'vue-router'
 
+import { registro } from '../../api/usuario'
+
 const router = useRouter()
 
-const modelo = ref()
+const modelo = ref({
+    nombre: '', email: '', descripcion: '', fecha_nacimiento: '', pass: '', repetir_pass: ''
+})
 
-function registro_p2(){
-    router.replace('registro_2')
+const present_loading = ref(false)
+const result_registro = ref()
+
+async function registro_p2(){
+    present_loading.value = true
+
+    if (modelo.value.nombre == '') {
+        alert('Es necesario especificar un Nombre.')
+        return false;
+    }
+
+    if (modelo.value.email == '') {
+        alert('Es necesario especificar un E-Mail.')
+        return false;
+    }
+
+    if (modelo.value.descripcion == '') {
+        alert('Es necesario especificar una Descripcion.')
+        return false;
+    }
+
+    if (modelo.value.fecha_nacimiento == '') {
+        alert('Es necesario especificar una Fecha de Nacimiento.')
+        return false;
+    }
+
+    if (modelo.value.password == '') {
+        alert('Es necesario especificar una Contraseña.')
+        return false;
+    }
+
+    if (modelo.value.repetir_pass == '') {
+        alert('Es necesario especificar la repetición de la Contraseña.')
+        return false;
+    }
+
+    result_registro.value = await registro( modelo.value )
+    if (result_registro.value.stat){
+        present_loading.value = false
+        router.replace('registro_2')
+    } else {
+        alert(present_loading.value.text)
+        present_loading.value = false
+    }
 }
 </script>
