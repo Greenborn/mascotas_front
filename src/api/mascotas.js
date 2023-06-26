5ea433f16d8568611d3a75d6f0a2092e8e5626fb
Luciano <lucho.2012.tandil@gmail.com>
2023-06-26 01:10:51 -0300

    definicion de endpoints

--- src/api/mascotas.js
+++ src/api/mascotas.js
@@ -5,3 +5,4 @@ const service = create(process.env.VUE_APP_BACKEND_URL)
 export function get_all() {  return service({ url: "/mascotas/get_all",  method: 'get'  }) }
 export function get_one( id ) {  return service({ url: "/mascotas/get?id="+id,  method: 'get'  }) }
 export function agregar( data ) { return service({  url: "/mascotas/agregar",    method: 'post', data  }) }
+export function editar( data ) { return service({  url: "/mascotas/editar",    method: 'put', data  }) }
