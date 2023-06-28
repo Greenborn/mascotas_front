6697fa535c884c5f6062987171b4cbae153f31aa
Luciano <lucho.2012.tandil@gmail.com>
2023-06-28 20:51:24 -0300

    Se avanza en vista de preguntas frecuentes agregando acordeones

--- src/views/dashboard/PreguntasFrecuentes.vue
+++ src/views/dashboard/PreguntasFrecuentes.vue
@@ -1,9 +1,16 @@
 <template>
     <ion-page id="main-content" class="def-fondo">
         <ion-grid>
-            <ion-row class="ion-justify-content-center ion-align-items-center vh-100" >
-                <ion-col size="auto">
-                    {{ listado_preguntas }}
+            <ion-row class="ion-justify-content-center  vh-100" >
+                <ion-col size-xs="12" size-sm="12" size-md="10" size-lg="8">
+                    <ion-accordion-group>
+                        <ion-accordion :value="faq.id" v-for="faq in listado_preguntas" :key="faq">
+                            <ion-item slot="header" color="light">
+                                <ion-label>{{ faq.pregunta }}</ion-label>
+                            </ion-item>
+                            <div class="ion-padding" slot="content">{{ faq.respuesta }}</div>
+                            </ion-accordion>
+                    </ion-accordion-group>
                 </ion-col>
             </ion-row>
         </ion-grid>
@@ -15,6 +22,7 @@
 import { ref, onMounted } from 'vue'
 import { IonItem, IonList } from '@ionic/vue' 
 import { IonCol, IonPage, IonGrid, IonRow, IonImg, IonCard, IonCardContent, IonCardHeader  } from '@ionic/vue';
+import { IonAccordion, IonAccordionGroup, IonLabel } from '@ionic/vue';
 import { IonLoading } from '@ionic/vue';
 
 import { preguntas_frecuentes } from '../../api/general'
