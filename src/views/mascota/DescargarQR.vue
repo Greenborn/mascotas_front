d817bbacc5f4cdf5b7190023a819d9c5902a93cf
Luciano <lucho.2012.tandil@gmail.com>
2023-07-22 16:36:30 -0300

    Se mejora visualización de vista de descarga de QR

--- src/views/mascota/DescargarQR.vue
+++ src/views/mascota/DescargarQR.vue
@@ -12,20 +12,12 @@
                                 </ion-col>
                             </ion-row>
                         </ion-card-header>
-
-                        <ion-card-content>
-                            <ion-grid>
-                                <ion-row class="ion-justify-content-center ion-align-items-center">
-                                    <ion-col size="auto"><ion-button @click="descargar_qr"><ion-icon slot="icon-only" :icon="cloudDownloadOutline"></ion-icon>&nbsp; Descargar</ion-button></ion-col>
-                                </ion-row>
-                            </ion-grid>
-                        </ion-card-content>
                     </ion-card>
 
                 </ion-col>
 
                 <ion-row>
-                    <ion-col size-xs="12">
+                    <ion-col size-xs="12" size-sm="10" size-md="6" size-lg="4">
                         <ion-card>
                             
                             <ion-card-content>
@@ -40,6 +32,11 @@
                                     <ion-row class="ion-justify-content-center ion-align-items-center">
                                         <ion-col  size="auto">
                                             Código: <b>{{ perfil_mascota_seleccionado.id }}</b>
+                                        </ion-col>
+                                    </ion-row>
+                                    <ion-row class="ion-justify-content-center ion-align-items-center">
+                                        <ion-col size="auto">
+                                            <ion-button @click="descargar_qr"><ion-icon slot="icon-only" :icon="cloudDownloadOutline"></ion-icon>&nbsp; Descargar</ion-button>
                                         </ion-col>
                                     </ion-row>
                                 </ion-grid>
