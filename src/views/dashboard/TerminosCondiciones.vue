8a568bfbfd5e1d77f3394be71b6aff004e198905
Luciano <lucho.2012.tandil@gmail.com>
2023-07-22 17:59:00 -0300

    Vista de terminos y condiciones lista

--- src/views/dashboard/TerminosCondiciones.vue
+++ src/views/dashboard/TerminosCondiciones.vue
@@ -2,8 +2,21 @@
     <ion-page id="main-content" class="def-fondo">
         <ion-grid>
             <ion-row class="ion-justify-content-center ion-align-items-center vh-100" >
-                <ion-col size="auto">
-                    {{ terminos_y_condiciones }}
+                <ion-col size-xs="12" size-sm="12" size-md="10" size-lg="6" >
+                    <ion-card>
+                        <ion-card-header>
+                            <ion-row>
+                                <ion-col>
+                                    <h2>Terminos y Condiciones</h2>
+                                </ion-col>
+                            </ion-row>
+                        </ion-card-header>
+
+                        <ion-card-content>
+                            <div class="w-100" v-html="terminos_y_condiciones.texto"></div>
+                        </ion-card-content>
+                    </ion-card>
+                    
                 </ion-col>
             </ion-row>
         </ion-grid>
@@ -13,8 +26,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { IonItem, IonList } from '@ionic/vue' 
-import { IonCol, IonPage, IonGrid, IonRow, IonImg, IonCard, IonCardContent, IonCardHeader  } from '@ionic/vue';
+import { IonCol, IonPage, IonGrid, IonRow, IonCard, IonCardContent, IonCardHeader  } from '@ionic/vue';
 import { IonLoading } from '@ionic/vue';
 
 import { legal } from '../../api/general'
@@ -26,8 +38,12 @@ onMounted(async ()=>{
     let res = undefined
     res = await legal()
     if (res?.stat){
-        terminos_y_condiciones.value = res.data[0]
-        console.log(res)
+        for (let c=0; c < res.data.length; c++)
+            if (res.data[c].id == 'terminos_y_condiciones') {
+                terminos_y_condiciones.value = res.data[c]
+                break
+            }
+
     } 
 })
 </script>
